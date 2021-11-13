476473c9da2a000afc06821b08eb44f744a596db
JhonLSN <jhonatanluansantos@gmail.com>
2021-11-13 19:26:32 -0300

    Bordeless

--- assets/js/user/script.js
+++ assets/js/user/script.js
@@ -1,31 +1,40 @@
 const btnUser = document.querySelector('.main-user__wrapper__authentication__acess');
+
 const optionPlane = document.querySelector('.main-user__wrapper__authentication__whatUser-traveler');
 const optionNative = document.querySelector('.main-user__wrapper__authentication__whatUser-native');
+
 const plane = document.querySelector('#plane');
 const planeIcon = document.querySelector('#plane i')
+
 const native = document.querySelector('#native');
 const nativeIcon = document.querySelector('#native i')
 
-optionPlane.addEventListener('mouseover', () =>{
+const check = document.querySelector('#check')
+
+optionPlane.addEventListener('click', () => {
   plane.style.background = '#08024b'
   planeIcon.style.color = '#FFF'
-})
-
-optionPlane.addEventListener('mouseout', () =>{
-  plane.style.background = '#FFF'
-  planeIcon.style.color = '#08024b'
-})
-
-optionNative.addEventListener('mouseover', () =>{
-  native.style.background = '#08024b'
-  nativeIcon.style.color = '#FFF'
-})
-
-optionNative.addEventListener('mouseout', () =>{
   native.style.background = '#FFF'
   nativeIcon.style.color = '#08024b'
+
 })
 
-btnUser.addEventListener('click', () =>{
+optionNative.addEventListener('click', () => {
+  native.style.background = '#08024b'
+  nativeIcon.style.color = '#FFF'
+  plane.style.background = '#FFF'
+  planeIcon.style.color = '#08024b'
 
+})
+
+btnUser.addEventListener('click', () => {
+  if(nativeIcon.style.color === '' || planeIcon.style.color === ''){
+    check.innerHTML = 'Selecione uma opção'
+    return;
+  }
+
+  alert('Sucesso!')
+  // btnUser.href = ''
 });
+
+
