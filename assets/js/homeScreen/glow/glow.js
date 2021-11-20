5381205c762a86a08db3de9f07d15d8d6eb3ad8b
JhonLSN <jhonatanluansantos@gmail.com>
2021-11-20 17:05:23 -0300

    Bordeless

---
+++ assets/js/homeScreen/glow/glow.js
@@ -0,0 +1,178 @@
+btnGlow.addEventListener('click', () => {
+  // Body
+  body.classList.remove('dark-mode')
+
+  // Header Variables
+  header.classList.remove('dark-mode')
+  logo.classList.remove('dark-mode')
+  inputHeader.classList.remove('dark-mode')
+  inputIcon.classList.remove('dark-mode')
+  userIcon.classList.remove('dark-mode')
+
+  // Navigation Variables
+  navigation.classList.remove('dark-mode')
+  navIconHouse.classList.remove('dark-mode')
+  navTextHouse.classList.remove('dark-mode')
+  navIconMessage.classList.remove('dark-mode')
+  navTextMessage.classList.remove('dark-mode')
+  navIconMap.classList.remove('dark-mode')
+  navTextMap.classList.remove('dark-mode')
+  navIconConfig.classList.remove('dark-mode')
+  navTextConfig.classList.remove('dark-mode')
+  navIconExit.classList.remove('dark-mode')
+  navTextExit.classList.remove('dark-mode')
+
+  // House Variables 
+  house.classList.remove('dark-mode')
+  houseSearchInput.classList.remove('dark-mode')
+  houseTitle.classList.remove('dark-mode')
+  houseSpText.classList.remove('dark-mode')
+  houseSpParagrph.classList.remove('dark-mode')
+  houseSpBtn.classList.remove('dark-mode')
+  houseMiamiText.classList.remove('dark-mode')
+  houseMiamiParagrph.classList.remove('dark-mode')
+  houseMiamiBtn.classList.remove('dark-mode')
+  houseLondresText.classList.remove('dark-mode')
+  houseLondresParagrph.classList.remove('dark-mode')
+  houseLondresBtn.classList.remove('dark-mode')
+  houseOrlandoText.classList.remove('dark-mode')
+  houseOrlandoParagrph.classList.remove('dark-mode')
+  houseOrlandoBtn.classList.remove('dark-mode')
+
+  // Aborde Variables
+  abode.classList.remove('dark-mode')
+  abodeFavorSp1.classList.remove('dark-mode')
+  abodeFavorSp1Description.classList.remove('dark-mode')
+  abodeFavorSp1BoldH1.classList.remove('dark-mode')
+  abodeFavorSp1BoldH2.classList.remove('dark-mode')
+  abodeFavorSp1BoldH3.classList.remove('dark-mode')
+  abodeFavorSp1BoldP.classList.remove('dark-mode')
+  abodeFavorSp2.classList.remove('dark-mode')
+  abodeFavorSp2Description.classList.remove('dark-mode')
+  abodeFavorSp2BoldH1.classList.remove('dark-mode')
+  abodeFavorSp2BoldH2.classList.remove('dark-mode')
+  abodeFavorSp2BoldH3.classList.remove('dark-mode')
+  abodeFavorSp2BoldP.classList.remove('dark-mode')
+  abodeFavorSp3.classList.remove('dark-mode')
+  abodeFavorSp3Description.classList.remove('dark-mode')
+  abodeFavorSp3BoldH1.classList.remove('dark-mode')
+  abodeFavorSp3BoldH2.classList.remove('dark-mode')
+  abodeFavorSp3BoldH3.classList.remove('dark-mode')
+  abodeFavorSp3BoldP.classList.remove('dark-mode')
+  abodeFavorSp4.classList.remove('dark-mode')
+  abodeFavorSp4Description.classList.remove('dark-mode')
+  abodeFavorSp4BoldH1.classList.remove('dark-mode')
+  abodeFavorSp4BoldH2.classList.remove('dark-mode')
+  abodeFavorSp4BoldH3.classList.remove('dark-mode')
+  abodeFavorSp4BoldP.classList.remove('dark-mode')
+  abodeFavorMiami1.classList.remove('dark-mode')
+  abodeFavorMiami1Description.classList.remove('dark-mode')
+  abodeFavorMiami1H1.classList.remove('dark-mode')
+  abodeFavorMiami1H2.classList.remove('dark-mode')
+  abodeFavorMiami1H3.classList.remove('dark-mode')
+  abodeFavorMiami1P.classList.remove('dark-mode')
+  abodeFavorMiami2.classList.remove('dark-mode')
+  abodeFavorMiami2Description.classList.remove('dark-mode')
+  abodeFavorMiami2H1.classList.remove('dark-mode')
+  abodeFavorMiami2H2.classList.remove('dark-mode')
+  abodeFavorMiami2H3.classList.remove('dark-mode')
+  abodeFavorMiami2P.classList.remove('dark-mode')
+  abodeFavorMiami3.classList.remove('dark-mode')
+  abodeFavorMiami3Description.classList.remove('dark-mode')
+  abodeFavorMiami3H1.classList.remove('dark-mode')
+  abodeFavorMiami3H2.classList.remove('dark-mode')
+  abodeFavorMiami3H3.classList.remove('dark-mode')
+  abodeFavorMiami3P.classList.remove('dark-mode')
+  abodeFavorMiami4.classList.remove('dark-mode')
+  abodeFavorMiami4Description.classList.remove('dark-mode')
+  abodeFavorMiami4H1.classList.remove('dark-mode')
+  abodeFavorMiami4H2.classList.remove('dark-mode')
+  abodeFavorMiami4H3.classList.remove('dark-mode')
+  abodeFavorMiami4P.classList.remove('dark-mode')
+  abodeFavorLondres1.classList.remove('dark-mode')
+  abodeFavorLondres1Description.classList.remove('dark-mode')
+  abodeFavorLondres1H1.classList.remove('dark-mode')
+  abodeFavorLondres1H2.classList.remove('dark-mode')
+  abodeFavorLondres1H3.classList.remove('dark-mode')
+  abodeFavorLondres1P.classList.remove('dark-mode')
+  abodeFavorLondres2.classList.remove('dark-mode')
+  abodeFavorLondres2Description.classList.remove('dark-mode')
+  abodeFavorLondres2H1.classList.remove('dark-mode')
+  abodeFavorLondres2H2.classList.remove('dark-mode')
+  abodeFavorLondres2H3.classList.remove('dark-mode')
+  abodeFavorLondres2P.classList.remove('dark-mode')
+  abodeFavorLondres3.classList.remove('dark-mode')
+  abodeFavorLondres3Description.classList.remove('dark-mode')
+  abodeFavorLondres3H1.classList.remove('dark-mode')
+  abodeFavorLondres3H2.classList.remove('dark-mode')
+  abodeFavorLondres3H3.classList.remove('dark-mode')
+  abodeFavorLondres3P.classList.remove('dark-mode')
+  abodeFavorLondres4.classList.remove('dark-mode')
+  abodeFavorLondres4Description.classList.remove('dark-mode')
+  abodeFavorLondres4H1.classList.remove('dark-mode')
+  abodeFavorLondres4H2.classList.remove('dark-mode')
+  abodeFavorLondres4H3.classList.remove('dark-mode')
+  abodeFavorLondres4P.classList.remove('dark-mode')
+  abodeFavorOrlando1.classList.remove('dark-mode')
+  abodeFavorOrlando1Description.classList.remove('dark-mode')
+  abodeFavorOrlando1H1.classList.remove('dark-mode')
+  abodeFavorOrlando1H2.classList.remove('dark-mode')
+  abodeFavorOrlando1H3.classList.remove('dark-mode')
+  abodeFavorOrlando1P.classList.remove('dark-mode')
+  abodeFavorOrlando2.classList.remove('dark-mode')
+  abodeFavorOrlando2Description.classList.remove('dark-mode')
+  abodeFavorOrlando2H1.classList.remove('dark-mode')
+  abodeFavorOrlando2H2.classList.remove('dark-mode')
+  abodeFavorOrlando2H3.classList.remove('dark-mode')
+  abodeFavorOrlando2P.classList.remove('dark-mode')
+  abodeFavorOrlando3.classList.remove('dark-mode')
+  abodeFavorOrlando3Description.classList.remove('dark-mode')
+  abodeFavorOrlando3H1.classList.remove('dark-mode')
+  abodeFavorOrlando3H2.classList.remove('dark-mode')
+  abodeFavorOrlando3H3.classList.remove('dark-mode')
+  abodeFavorOrlando3P.classList.remove('dark-mode')
+  abodeFavorOrlando4.classList.remove('dark-mode')
+  abodeFavorOrlando4Description.classList.remove('dark-mode')
+  abodeFavorOrlando4H1.classList.remove('dark-mode')
+  abodeFavorOrlando4H2.classList.remove('dark-mode')
+  abodeFavorOrlando4H3.classList.remove('dark-mode')
+  abodeFavorOrlando4P.classList.remove('dark-mode')
+
+  //PopUp Variables
+  popUp.classList.remove('dark-mode')
+  popUpAccont.classList.remove('dark-mode')
+  popUpAddress.classList.remove('dark-mode')
+  popUpHelp.classList.remove('dark-mode')
+  popUpBack.classList.remove('dark-mode')
+  popUpNavigationRow.classList.remove('dark-mode')
+  popUpNavigationRowName.classList.remove('dark-mode')
+  popUpNavigationRowPhone.classList.remove('dark-mode')
+  popUpNavigationRowEmail.classList.remove('dark-mode')
+  popUpNavigationRowPassword.classList.remove('dark-mode')
+
+  // Event 'click' Navigation PopUp
+  btnAccont.addEventListener('click', () =>{
+    btnPanelAddress.style.display = 'none'
+    btnPanelAccont.style.display = 'block'
+    btnAccont.style.pointerEvents = 'none'
+    btnAddress.style.pointerEvents = 'all'
+    IconAccont.style.color = '#08024b'
+    PAccont.style.color = '#08024b'
+    IconAddress.style.color = '#90e0ef'
+    PAddress.style.color = '#90e0ef'
+  })
+  
+  btnAddress.addEventListener('click', () =>{
+    btnPanelAddress.style.display = 'block'
+    btnPanelAccont.style.display = 'none'
+    btnAccont.style.pointerEvents = 'all'
+    btnAddress.style.pointerEvents = 'none'
+    IconAddress.style.color = '#08024b'
+    PAddress.style.color = '#08024b'
+    IconAccont.style.color = '#90e0ef'
+    PAccont.style.color = '#90e0ef'
+  })
+
+  btnDark.style.display = 'block'
+  btnGlow.style.display = 'none'
+})
