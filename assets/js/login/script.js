4f6ebe3a4bbfe217f00361c12a45643831fe238d
JhonLSN <jhonatanluansantos@gmail.com>
2021-11-20 18:47:21 -0300

    Bordeless

--- assets/js/login/script.js
+++ assets/js/login/script.js
@@ -1,7 +1,7 @@
 const btnLogin = document.querySelector('.main-login__wrapper__authentication__acess')
 const inputlogin = document.querySelector('.main-login__wrapper__authentication__user-input');
 const passwordLogin = document.querySelector('.main-login__wrapper__authentication__password-input');
-const check = document.querySelector('#check');
+const check = document.querySelector('.check');
 
 btnLogin.addEventListener('click', () =>{
   if(inputlogin.value === '' || passwordLogin.value === ''){
