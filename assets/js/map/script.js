7ab4e9f4eff0dba10f0b1410a04545420f2603b4
JhonLSN <jhonatanluansantos@gmail.com>
2021-11-24 19:00:14 -0300

    Bordeless

--- assets/js/map/script.js
+++ assets/js/map/script.js
@@ -1,27 +1,29 @@
+const graphicIlustration = document.querySelector('.main__wrapper__graphic__ilustration')
 const graphicPlane = document.querySelector('.main__wrapper__graphic-plane')
 const graphicBank = document.querySelector('.main__wrapper__graphic-bank')
+const graphicConstruction = document.querySelector('.main__wrapper__graphic-construction')
 const graphicEmail = document.querySelector('.main__wrapper__graphic-email')
 const graphicSchool = document.querySelector('.main__wrapper__graphic-school')
 const graphicFarmacy = document.querySelector('.main__wrapper__graphic-farmacy')
 const graphicHospital = document.querySelector('.main__wrapper__graphic-hospital')
 const graphicChurch = document.querySelector('.main__wrapper__graphic-church')
-const graphicConstruction = document.querySelector('.main__wrapper__graphic-construction')
 const graphicWrench = document.querySelector('.main__wrapper__graphic-wrench')
 const graphicShopping = document.querySelector('.main__wrapper__graphic-shopping')
 const graphicGraduation = document.querySelector('.main__wrapper__graphic-graduation')
 
 placesPlane.addEventListener('click', () => {
-  graphicPlane
-  graphicBank
-  graphicEmail
-  graphicSchool
-  graphicFarmacy
-  graphicHospital
-  graphicChurch
-  graphicConstruction
-  graphicWrench
-  graphicShopping
-  graphicGraduation
+  graphicIlustration.style.display = 'none'
+  graphicPlane.style.display = 'block'
+  graphicBank.style.display = 'none'
+  graphicConstruction.style.display = 'none'
+  graphicEmail.style.display = 'none'
+  graphicSchool.style.display = 'none'
+  graphicFarmacy.style.display = 'none'
+  graphicHospital.style.display = 'none'
+  graphicChurch.style.display = 'none'
+  graphicWrench.style.display = 'none'
+  graphicShopping.style.display = 'none'
+  graphicGraduation.style.display = 'none'
 })
 
 placesBank.addEventListener('click', () => {
