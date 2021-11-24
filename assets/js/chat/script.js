14ef27aadabb80328251898d5c6a06cee4936b87
JhonLSN <jhonatanluansantos@gmail.com>
2021-11-24 14:13:48 -0300

    Bordeless

--- assets/js/chat/script.js
+++ assets/js/chat/script.js
@@ -1,16 +1,20 @@
-// Support Variables 
+// Support Variable 
 const buttonEraserSupport = document.querySelector('.main__wrapper__chat__support-eraser i')
 
 // User Variables
 const buttonEraserUser = document.querySelector('.main__wrapper__chat__user-eraser i')
 
-// New Chat variables
+// New Chat Variable
 const newChat = document.querySelector('.new-chat')
+
+// Illustration Variable
+const sendIlustration = document.querySelector('.main__wrapper__send__ilustration')
 
 // Event Support
 chatSupportBox.addEventListener('click', () =>{
   sendSupport.style.display = 'block'
   sendNewUser.style.display = 'none'
+  sendIlustration.style.display = 'none'
 })
 
 buttonEraserSupport.addEventListener('click', () => {
@@ -23,6 +27,7 @@ buttonEraserSupport.addEventListener('click', () => {
 chatNewUserBox.addEventListener('click', () =>{
   sendNewUser.style.display = 'block'
   sendSupport.style.display = 'none'
+  sendIlustration.style.display = 'none'
 })
 
 buttonEraserUser.addEventListener('click', () => {
