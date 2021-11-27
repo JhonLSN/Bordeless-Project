9d0a8ff795a3399187d9334349613cd7c58992e4
JhonLSN <jhonatanluansantos@gmail.com>
2021-11-27 15:31:52 -0300

    Bordeless

--- assets/js/start/dark/dark.js
+++ assets/js/start/dark/dark.js
@@ -4,12 +4,14 @@ const btnGlow = document.querySelector('.glow')
 // Main 
 const mainStartBackground = document.querySelector('.main-start')
 const mainStartBtnAnchor = document.querySelector('.main-start__wrapper-anchor')
+const mainStartTitle = document.querySelector('.main-start__wrapper__authentication__title')
 const mainStartAcces = document.querySelector('.main-start__wrapper__authentication__acess')
 
 btnDark.addEventListener('click', () => {
   // Main Variables 
   mainStartBackground.classList.add('dark-mode')
   mainStartBtnAnchor.classList.add('dark-mode')
+  mainStartTitle.classList.add('dark-mode')
   mainStartAcces.classList.add('dark-mode')
 
   btnDark.style.display = 'none'
--- assets/js/start/glow/glow.js
+++ assets/js/start/glow/glow.js
@@ -2,6 +2,7 @@ btnGlow.addEventListener('click', () => {
   // Main Variables 
   mainStartBackground.classList.add('dark-mode')
   mainStartBtnAnchor.classList.add('dark-mode')
+  mainStartTitle.classList.add('dark-mode')
   mainStartAcces.classList.add('dark-mode')
 
   btnDark.style.display = 'block'
