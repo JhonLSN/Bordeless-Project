6a33054ecd384d0a7758ef933a6ced47b41c26e2
JhonLSN <jhonatanluansantos@gmail.com>
2021-11-27 15:24:27 -0300

    Bordeless

--- assets/js/start/glow/glow.js
+++ assets/js/start/glow/glow.js
@@ -1,15 +1,8 @@
 btnGlow.addEventListener('click', () => {
   // Main Variables 
-  mainLoginBackground.classList.remove('dark-mode')
-  mainLoginBtnAnchor.classList.remove('dark-mode')
-  mainLoginTitle.classList.remove('dark-mode')
-  mainLoginUserIcon.classList.remove('dark-mode')
-  mainLoginUserInput.classList.remove('dark-mode')
-  mainLoginPasswordIcon.classList.remove('dark-mode')
-  mainLoginPasswordInput.classList.remove('dark-mode')
-  mainLoginCheck.classList.remove('dark-mode')
-  mainLoginAcces.classList.remove('dark-mode')
-  mainLoginCreatedAccont.classList.remove('dark-mode')
+  mainStartBackground.classList.add('dark-mode')
+  mainStartBtnAnchor.classList.add('dark-mode')
+  mainStartAcces.classList.add('dark-mode')
 
   btnDark.style.display = 'block'
   btnGlow.style.display = 'none'
